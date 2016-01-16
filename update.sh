1bb9d4557cbd7c7570c40069cec7ac1ade9a30df
ant31 <2t.antoine@gmail.com>
2016-01-16 23:09:37 +0100

    Initial

---
+++ update.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+version="$1"
+md5="$2"
+check_result=0
+check_tar()
+{
+
+    echo "tar"
+    if [ -f kubernetes.tar.gz ]; then
+        kmd5=`md5sum kubernetes.tar.gz | cut -f1 -d" " `
+        echo $kmd5 $md5
+        if [ "$md5" == "$kmd5" ]; then
+            check_result=1
+        fi
+    fi
+}
+
+download_hyperkube()
+{
+    mkdir -p kube-binaries
+
+    check_tar
+    echo $check_result
+    if [ $check_result -eq 0 ]; then
+        curl -L -v "https://github.com/kubernetes/kubernetes/releases/download/$version/kubernetes.tar.gz" -o kubernetes.tar.gz
+        tar xvf kubernetes.tar.gz
+        tar xvf kubernetes/server/kubernetes-server-linux-amd64.tar.gz -C kube-binaries
+    else
+        echo "Skip download"
+     fi
+    cp kube-binaries/kubernetes/server/bin/hyperkube build
+}
+
+download_hyperkube
+git add ./build && git commit -m "Hyperkube $version" && git tag $version -f
